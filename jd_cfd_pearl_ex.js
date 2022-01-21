6251896e68fef4f0337b0902d2f46d285288ec1f
yunyan6 <68782672+yunyan6@users.noreply.github.com>
2022-01-21 11:05:19 +0800

    富岛珍珠兑换

--- jd_cfd_pearl_ex.js
+++ jd_cfd_pearl_ex.js
@@ -27,6 +27,7 @@ console.log(`默认红包余额大于0.2元就参与兑换\n`);
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
+	
   $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
   await requestAlgo();
   await $.wait(1000)
